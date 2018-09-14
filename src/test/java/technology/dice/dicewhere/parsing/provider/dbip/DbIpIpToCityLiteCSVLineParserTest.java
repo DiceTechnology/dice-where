25229948ab282583696f23dc95afb0e835ec2313
Gonçalo Luiz <goncalo.luiz@gmail.com>
2018-09-14 16:58:54 +0100

    more precise class name to reflect the correct db-ip database

--- src/test/java/technology/dice/dicewhere/parsing/provider/dbip/DbIpIpToCityLiteCSVLineParserTest.java
+++ src/test/java/technology/dice/dicewhere/parsing/provider/dbip/DbIpIpToCityLiteCSVLineParserTest.java
@@ -13,17 +13,17 @@ import technology.dice.dicewhere.api.api.IP;
 import technology.dice.dicewhere.api.api.IpInformation;
 import technology.dice.dicewhere.api.exceptions.LineParsingException;
 import technology.dice.dicewhere.parsing.ParsedLine;
-import technology.dice.dicewhere.provider.dbip.parsing.DbIpIpToCityCSVLineParser;
+import technology.dice.dicewhere.provider.dbip.parsing.DbIpIpToCityLiteCSVLineParser;
 import technology.dice.dicewhere.provider.dbip.parsing.DbIpIpToLocationAndIspCSVLineParser;
 import technology.dice.dicewhere.reading.RawLine;
 
-public class DbIpIpToCityCSVLineParserTest {
+public class DbIpIpToCityLiteCSVLineParserTest {
   @Test
   public void ipV4LineWithOriginalLine() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line = "1.8.154.0,1.8.155.255,AS,CN,Beijing,Beijing,39.904,116.408";
     RawLine rawLine = new RawLine(line, 1);
-    ParsedLine parsed = dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), true);
+    ParsedLine parsed = dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), true);
     ParsedLine expected =
         new ParsedLine(
             new IP(InetAddresses.forString("1.8.154.0")),
@@ -44,10 +44,10 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test
   public void ipV4LineWithoutOriginalLine() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line = "1.8.154.0,1.8.155.255,AS,CN,Beijing,Beijing,39.904,116.408";
     RawLine rawLine = new RawLine(line, 1);
-    ParsedLine parsed = dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), false);
+    ParsedLine parsed = dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), false);
     ParsedLine expected =
         new ParsedLine(
             new IP(InetAddresses.forString("1.8.154.0")),
@@ -68,11 +68,11 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test
   public void ipV6LineWithOriginal() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line =
         "2c0f:ff80::,2c0f:ff80:ffff:ffff:ffff:ffff:ffff:ffff,AF,ZA,Gauteng,\"Sandton (Woodmead)\",-26.0561,28.0696";
     RawLine rawLine = new RawLine(line, 1);
-    ParsedLine parsed = dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), true);
+    ParsedLine parsed = dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), true);
     ParsedLine expected =
         new ParsedLine(
             new IP(InetAddresses.forString("2C0F:FF80:0000:0000:0000:0000:0000:0000")),
@@ -93,11 +93,11 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test
   public void ipV6LineWithWithoutOriginalLine() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line =
         "2c0f:ff80::,2c0f:ff80:ffff:ffff:ffff:ffff:ffff:ffff,AF,ZA,Gauteng,\"Sandton (Woodmead)\",-26.0561,28.0696";
     RawLine rawLine = new RawLine(line, 1);
-    ParsedLine parsed = dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), false);
+    ParsedLine parsed = dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), false);
     ParsedLine expected =
         new ParsedLine(
             new IP(InetAddresses.forString("2C0F:FF80:0000:0000:0000:0000:0000:0000")),
@@ -118,10 +118,10 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test(expected = LineParsingException.class)
   public void wrongLineFormat() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line = "column1,column2,column3";
     try {
-      dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), false);
+      dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), false);
     } catch (LineParsingException e) {
       Assert.assertEquals(line, e.getOffendingLine().getLine());
       Assert.assertEquals(1, e.getOffendingLine().getLineNumber());
@@ -131,12 +131,12 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test(expected = LineParsingException.class)
   public void wrongStartIpFormat() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
 
     String line =
         "uh-Oh,1.0.0.255,AU,Queensland,Brisbane,\"South Brisbane\",4101,-27.4748,153.017,2207259,10,Australia/Brisbane,\"APNIC Research and Development\",,";
     try {
-      dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), false);
+      dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), false);
     } catch (LineParsingException e) {
       Assert.assertEquals(line, e.getOffendingLine().getLine());
       Assert.assertEquals(1, e.getOffendingLine().getLineNumber());
@@ -146,11 +146,11 @@ public class DbIpIpToCityCSVLineParserTest {
 
   @Test(expected = LineParsingException.class)
   public void wrongEndIpFormat() throws LineParsingException {
-    DbIpIpToCityCSVLineParser dbIpIpToCityCSVLineParser = new DbIpIpToCityCSVLineParser();
+    DbIpIpToCityLiteCSVLineParser dbIpIpToCityLiteCSVLineParser = new DbIpIpToCityLiteCSVLineParser();
     String line =
         "1.0.0.0,stop-Hammertime,AU,Queensland,Brisbane,\"South Brisbane\",4101,-27.4748,153.017,2207259,10,Australia/Brisbane,\"APNIC Research and Development\",,";
     try {
-      dbIpIpToCityCSVLineParser.parse(new RawLine(line, 1), false);
+      dbIpIpToCityLiteCSVLineParser.parse(new RawLine(line, 1), false);
     } catch (LineParsingException e) {
       Assert.assertEquals(line, e.getOffendingLine().getLine());
       Assert.assertEquals(1, e.getOffendingLine().getLineNumber());
