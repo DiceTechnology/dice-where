5657a3da9555db94b2dd041aca497dbd585054ef
Stan <me@iamstan.net>
2018-09-21 16:07:05 +0100

    decorator tests and maxmind implementation

--- src/test/java/technology/dice/dicewhere/decorator/VpnDecoratorTest.java
+++ src/test/java/technology/dice/dicewhere/decorator/VpnDecoratorTest.java
@@ -12,66 +12,24 @@ import org.junit.Assert;
 import org.junit.Test;
 import technology.dice.dicewhere.api.api.IP;
 import technology.dice.dicewhere.api.api.IpInformation;
-import technology.dice.dicewhere.provider.maxmind.decorator.MaxmindVpnDecoratorDbReader;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class VpnDecoratorTest {
 
-  private String IPv4_LINES =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node\n"
-          + "1.0.1.0/28,1,1,0,0,0\n"
-          + "1.0.1.16/28,1,1,0,0,0\n"
-          + "1.0.2.16/28,1,1,0,0,0\n"
-          + "1.0.2.64/28,1,1,0,0,0\n"
-          + "1.0.3.16/28,1,1,0,0,0\n"
-          + "1.0.3.32/28,1,0,0,0,0\n"
-          + "1.0.3.64/28,1,1,0,0,0\n"
-          + "1.0.4.0/28,1,1,0,0,0\n"
-          + "1.0.4.16/28,1,1,0,0,0\n"
-          + "1.0.5.0/28,1,1,0,0,0\n"
-          + "1.0.5.32/28,1,1,0,0,0";
-
-  private String IPv6_LINES =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node\n"
-          + "2001:470:7:600::/55,1,1,0,0,0\n"
-          + "2001:470:7:a00::/56,1,1,0,0,0\n"
-          + "2001:470:7:b70::/62,1,1,0,0,0\n"
-          + "2001:470:8:80::/59,1,1,0,0,0";
-  private String IPv4_LINES_2 =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node\n"
-          + "1.0.2.32/28,1,0,0,0,0\n"
-          + "1.0.2.55/32,1,1,0,0,0\n"
-          + "1.0.2.64/28,1,1,0,0,0\n"
-          + "1.0.4.0/27,1,1,0,0,0\n"
-          + "1.0.5.0/27,1,1,0,0,0";
-  private String IPv6_LINES_2 =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node";
-
-  private String IPv4_LINES_3 =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node\n"
-          + "1.0.2.16/28,1,1,0,0,0\n"
-          + "1.0.2.64/28,1,1,0,0,0\n"
-          + "1.0.3.64/28,1,1,0,0,0\n"
-          + "1.0.4.0/26,1,1,0,0,0\n"
-          + "1.0.5.0/26,1,1,0,0,0";
-
-  private String IPv6_LINES_3 =
-      "network,is_anonymous,is_anonymous_vpn,is_hosting_provider,is_public_proxy,is_tor_exit_node";
 
   @Test(expected = NullPointerException.class)
   public void shouldThrowNpe() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     decorator.decorate(null);
   }
 
   @Test
   public void shouldDecorateMultipleOverlappingRanges_anyMergeStrategy() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     IPAddress inputAddress = new IPAddressString("1.0.4.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -100,7 +58,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldDecorateMultipleOverlappingRanges_majorityMergeStrategy() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.MAJORITY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.MAJORITY);
     IPAddress inputAddress = new IPAddressString("1.0.5.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -129,7 +87,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnOriginalInfoCompletelyDecorated_wholeRegionIsVpn() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     IPAddress inputAddress = new IPAddressString("1.0.1.0/27").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -152,7 +110,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnOriginalInfo_noVpnRangesMatch() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     IPAddress inputAddress = new IPAddressString("1.0.6.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -167,7 +125,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnOriginalInfoDecorated_matchesExactlyARange() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     IPAddress inputAddress = new IPAddressString("1.0.2.16/28").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -184,7 +142,7 @@ public class VpnDecoratorTest {
   @Test
   public void shouldReturnOriginalInfoNotDecorated_doesntMatchDecorationStrategy()
       throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ALL);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ALL);
     IPAddress inputAddress = new IPAddressString("1.0.2.16/28").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -199,7 +157,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnRangesThatExistInAllDecorators() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ALL);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ALL);
     IPAddress inputAddress = new IPAddressString("1.0.2.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -233,7 +191,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnRangesThatExistInMajoritylDecorators() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.MAJORITY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.MAJORITY);
     IPAddress inputAddress = new IPAddressString("1.0.2.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -279,7 +237,7 @@ public class VpnDecoratorTest {
 
   @Test
   public void shouldReturnRangesFromBothDecorators() throws IOException {
-    VpnDecorator decorator = getDecorator(DecorationStrategy.ANY);
+    VpnDecorator decorator = DecoratorTestUtils.getVpnDecorator(DecorationStrategy.ANY);
     IPAddress inputAddress = new IPAddressString("1.0.2.0/24").getAddress();
     IpInformation target =
         IpInformation.builder()
@@ -336,25 +294,7 @@ public class VpnDecoratorTest {
     Assert.assertEquals(expected, actual);
   }
 
-  private VpnDecorator getDecorator(DecorationStrategy strategy) throws IOException {
-    return new VpnDecorator(
-        Stream.<MaxmindVpnDecoratorDbReader>builder()
-            .add(getDecorator(IPv4_LINES, IPv6_LINES))
-            .add(getDecorator(IPv4_LINES_2, IPv6_LINES_2))
-            .add(getDecorator(IPv4_LINES_3, IPv6_LINES_3))
-            .build()
-            .collect(Collectors.toList()),
-        strategy);
-  }
 
-  private MaxmindVpnDecoratorDbReader getDecorator(String ipv4Lines, String ipv6Lines)
-      throws IOException {
-    InputStream streamV4 = new ByteArrayInputStream(ipv4Lines.getBytes());
-    BufferedReader bufferedReaderV4 = new BufferedReader(new InputStreamReader(streamV4));
-    InputStream streamV6 = new ByteArrayInputStream(ipv6Lines.getBytes());
-    BufferedReader bufferedReaderV6 = new BufferedReader(new InputStreamReader(streamV6));
-    return new MaxmindVpnDecoratorDbReader(bufferedReaderV4, bufferedReaderV6);
-  }
 
   IP getLowerFromIP(String input) {
     return new IP(new IPAddressString(input).getAddress().getLower().getBytes());
