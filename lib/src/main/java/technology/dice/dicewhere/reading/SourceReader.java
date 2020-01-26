4346dcbc21fe7d7d362c50f84ccc48dabe0a4925
Gonçalo Luiz <goncalo.luiz@gmail.com>
2020-01-26 20:00:37 +0000

    add copywrite headers

--- lib/src/main/java/technology/dice/dicewhere/reading/SourceReader.java
+++ lib/src/main/java/technology/dice/dicewhere/reading/SourceReader.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2018 - present by Dice Technology Ltd.
+ *
+ * Please see distribution for license.
+ */
 package technology.dice.dicewhere.reading;
 
 import technology.dice.dicewhere.building.DatabaseBuilderListener;
