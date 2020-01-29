5939cc295254d49804a68f03310e2541d591399f
Gonçalo Luiz <goncalo.luiz@gmail.com>
2020-01-29 10:06:37 +0000

    add localData directory

---
+++ lib/src/test/java/technology/dice/dicewhere/building/maxmindnative/MaxmindNativeDatabaseTestIT.java
@@ -0,0 +1,10 @@
+package technology.dice.dicewhere.building.maxmindnative;
+
+import static org.junit.Assert.*;
+
+// As per maxmind T&Cs we are not allowed to re-distribute a Maxmind database. Therefore, the test
+// resources necessary to run these tests cannot be uploaded to source control
+// Please set the
+public class MaxmindNativeDatabaseTestIT {
+
+}
