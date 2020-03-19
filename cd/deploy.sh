2246da468a8f72ed67203072f89e8c6a12b81ff4
Kamil Włodarczyk <kwlodarczyk@endeavorco.com>
2020-03-19 12:49:26 +0000

    Copy script to have relative paths.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,10 +2,12 @@
 set -x 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   cd dice-where
-  ../cd/version.sh
+  cp ../cd/version.sh .
+  ./version.sh
   mvn deploy -P bintray --settings ../cd/mvnsettings.xml
   cd ../dice-where-downloader
-  ../cd/version.sh
+  cp ../cd/version.sh .
+  ./version.sh
   mvn deploy -P bintray --settings ../cd/mvnsettings.xml
   cd ..
   ./cd/tag.sh
