9aebe1d1514f5d43d4eeb9398500a1745d1a8641
Kamil Włodarczyk <kwlodarczyk@endeavorco.com>
2020-03-19 14:52:36 +0000

    Do local install of the parent project.

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -2,18 +2,29 @@
 set -x 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
   ./cd/version.sh
+
   cd dice-where
   cp ../cd/version.sh .
   ./version.sh
-  mvn deploy -P bintray --settings ../cd/mvnsettings.xml
   cd ../dice-where-downloader-lib
   cp ../cd/version.sh .
   ./version.sh
-  mvn clean install
   cd ../dice-where-downloader
   cp ../cd/version.sh .
   ./version.sh
+  cd ..
+
+  mvn clean install -DskipTests -q
+
+  cd dice-where-downloader-lib
+  mvn clean install -DskipTests -q
+  cd ..
+
+  cd dice-where
+  mvn deploy -P bintray --settings ../cd/mvnsettings.xml
+  cd ../dice-where-downloader
   mvn deploy -P bintray --settings ../cd/mvnsettings.xml
   cd ..
+
   ./cd/tag.sh
 fi
