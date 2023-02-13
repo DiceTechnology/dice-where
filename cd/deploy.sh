4ad3422da03d8c1b8c395baa19d64cde43e2ad21
Miguel Pontes <mpontes@users.noreply.github.com>
2023-02-13 17:06:07 +0000

    Fix tagging for GH Actions

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x 
 
-echo $GPG_KEY | gpg --batch --fast-import gpg.asc
+echo $GPG_KEY | gpg --batch --fast-import
 
 ./cd/version.sh
 
@@ -16,13 +16,13 @@ cp ../cd/version.sh .
 ./version.sh
 cd ..
 
-mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml
+mvn deploy -P publish -DskipTests=true --settings ../cd/mvnsettings.xml
 cd dice-where
-mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml
+mvn deploy -P publish -DskipTests=true --settings ../cd/mvnsettings.xml
 cd ../dice-where-downloader-lib
-mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml
+mvn deploy -P publish -DskipTests=true --settings ../cd/mvnsettings.xml
 cd ../dice-where-downloader
-mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml
+mvn deploy -P publish -DskipTests=true --settings ../cd/mvnsettings.xml
 cd ..
 
 ./cd/tag.sh
