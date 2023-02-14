cf13fd794b74c21abda3aff45ee9916c26c79f71
Miguel Pontes <mpontes@users.noreply.github.com>
2023-02-14 11:35:57 +0000

    Fix Docker Hub credentials not being picked up (#96)
    Co-authored-by: DiceTech CI <build@dice.technology>

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -5,6 +5,7 @@ echo $GPG_KEY | base64 --decode | gpg --batch --fast-import
 set -x
 
 ./cd/version.sh && \
-mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml && \
-./cd/tag.sh
+./cd/tag.sh && \
+mvn deploy -P publish -DskipTests=true --settings cd/mvnsettings.xml
+
 
