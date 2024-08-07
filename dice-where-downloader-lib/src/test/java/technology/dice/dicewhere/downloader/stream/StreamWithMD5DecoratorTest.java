fb71485b46032ea70839e3283d287ca57d93c782
Diogo Santos <snackk026@gmail.com>
2024-08-07 15:59:46 +0100

    ID-1570 - reverted pom;

--- dice-where-downloader-lib/src/test/java/technology/dice/dicewhere/downloader/stream/StreamWithMD5DecoratorTest.java
+++ dice-where-downloader-lib/src/test/java/technology/dice/dicewhere/downloader/stream/StreamWithMD5DecoratorTest.java
@@ -1,6 +1,6 @@
 package technology.dice.dicewhere.downloader.stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,15 +9,14 @@ import java.nio.charset.Charset;
 import java.nio.file.Path;
 import java.security.NoSuchAlgorithmException;
 import org.apache.commons.io.IOUtils;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+
 
 public class StreamWithMD5DecoratorTest {
 
   private static final String PATH = "/maxmind/maxmind-city-1.zip";
 
   @Test
-  @DisplayName("Should Successfully Read a Stream and calculate the entire stream digest")
   public void shouldSuccessfullyReadAndCalculateDigestOfStream()
       throws IOException, NoSuchAlgorithmException, URISyntaxException {
     Path path = Path.of(getClass().getResource(PATH).toURI());
